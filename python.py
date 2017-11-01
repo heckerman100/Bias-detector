299f05667494badd158a8319a126f42b415eab32
Heckerman1000 <huntermcnulty@gmail.com>
2017-11-01 19:05:41 -0400

    Update python.py

--- python.py
+++ python.py
@@ -2,128 +2,114 @@
 #Not 100% acurate
 
 
-#Non bias words
-they = 0
-male = 0
-female = 0
-buisness = 0
-police = 0
-mother = 0
-father = 0
-brother = 0
-sister = 0
-uncle = 0
-aunt = 0
-grandparents = 0
-grandmother = 0
-grandfather = 0
-we = 0
-server = 0
-elderly = 0
-
 
 #Bias words
-their = 1
+Their = 1
 I = 1
-culture = 1
-race = 1
-kingsize = 1
-buisnessman = 1
-career girl = 1
-career woman = 1
-foreman = 1
-mailman = 1
-landlord = 1
-landlady = 1
-newsman = 1
-repairman = 1
-policeman = 1
-salesman = 1
-saleswoman = 1
-saleslady = 1
-cleaning lady = 1
-paper boy = 1
-waitress = 1
-workman = 1
-spokesman = 1
-ladylike = 1
-manly = 1
-maiden = 1
-manhood = 1
-manmade = 1
-motherly = 1
-statesman = 1
-congressman = 1
-congresswoman = 1
-chairmen = 1
-craftsman = 1
-old people = 1
+Culture = 1
+Race = 1
+Kingsize = 1
+Buisnessman = 1
+Careergirl = 1
+Careerwoman = 1
+Foreman = 1
+Mailman = 1
+Landlord = 1
+Landlady = 1
+Newsman = 1
+Repairman = 1
+Policeman = 1
+Salesman = 1
+Saleswoman = 1
+Saleslady = 1
+Cleaninglady = 1
+Paperboy = 1
+Waitress = 1
+Workman = 1
+Spokesman = 1
+Ladylike = 1
+Manly = 1
+Maiden = 1
+Manhood = 1
+Manmade = 1
+Motherly = 1
+Statesman = 1
+Congressman = 1
+Congresswoman = 1
+Chairmen = 1
+Craftsman = 1
+Oldpeople = 1
 Eskimo = 1
-fireman = 1
-illegal alien = 1
+Fireman = 1
+Illegalalien = 1
 Indian = 1
 Jew = 1
-mankind = 1
-underprivileged = 1
-wealthy = 1
-south side = 1
-blue collar = 1
-inner city = 1
-waiter = 1
+Mankind = 1
+Underprivileged = 1
+Wealthy = 1
+Southside = 1
+Bluecollar = 1
+Innercity = 1
+Waiter = 1
 
 #Original value
-Original_value = 
+Original_value = 0
 
 #Input text
 Text = input("Paste text or type here: ")
 
-
-
-
 #Applied value
+App1 = Text.count('I')
+App2 = Text.count('Their')
+App3 = Text.count('Culture')
+App4 = Text.count('Race')
+App5 = Text.count('Kingsize')
+App6 = Text.count('Buisnessman')
+App7 = Text.count('Careergirl')
+App8 = Text.count('Congresswoman')
+App9 = Text.count('Foreman')
+App10 = Text.count('Mailman')
+App11 = Text.count('Landlord')
+App12 = Text.count('Landlady')
+App13 = Text.count('Newsman')
+App14 = Text.count('Repairman')
+App15 = Text.count('Policeman')
+App16 = Text.count('Saleswoman')
+App17 = Text.count('Salesman')
+App18 = Text.count('Saleslady')
+App19 = Text.count('Cleaninglady')
+App20 = Text.count('Paperboy')
+App21 = Text.count('Waitress')
+App22 = Text.count('Workman')
+App23 = Text.count('Spokesman')
+App24 = Text.count('Ladylike')
+App25 = Text.count('Manly')
+App26 = Text.count('Maiden')
+App27 = Text.count('Manhood')
+App28 = Text.count('Manmade')
+App29 = Text.count('Motherly')
+App30 = Text.count('Statesman')
+App31 = Text.count('Congressman')
+App32 = Text.count('Congresswoman')
+App33 = Text.count('Chairmen')
+App34 = Text.count('Craftsman')
+App35 = Text.count('Oldpeople')
+App36 = Text.count('Eskimo')
+App37 = Text.count('Fireman')
+App38 = Text.count('Illegalalien')
+App39 = Text.count('Indian')
+App40 = Text.count('Jew')
+App41 = Text.count('Mankind')
+App42 = Text.count('Underprivileged')
+App43 = Text.count('Wealthy')
+App44 = Text.count('Southside')
+App45 = Text.count('Bluecollar')
+App46 = Text.count('Innercity')
+App47 = Text.count('Waiter')
 
-
-
+Applied_value = App1 + App2 + App3 + App4 + App5 + App6 + App7 + App8 + App9 + App10 + App11 + App12 + App13 + App14 + App15 + App16 + App17 + App18 + App19 + App20 + App21 + App22 + App23 + App24 + App25 + App26 + App27 + App28 + App29 + App30 + App31 + App32 + App33 + App34 + App35 + App36 + App37 + App38 + App39 + App40 + App41 + App42 + App43 + App44 + App45 + App46 + App47
 
 #Total
 Total = Original_value - Applied_value
-
-#Bias
-if Total = '-10':
-	print("This text is probably bias")
-if Total = '-11':
-	Print("This text is probably bias")
-if Total = '-12':
-	Print("This text is probably bias")
-if Total = '-13':
-	Print("This text is probably bias")
-if Total = '-14':
-	Print("This text is probably bias")
-if Total = '-15':
-	Print("This text is probably bias")
-if Total = '-16':
-	Print("This text is probably bias")
-if Total = '-17':
-	Print("This text is probably bias")
-
-
-
-#Non Bias
-if Total = '-9':
-	print("This text is probably not bias")
-if Total = '-8':
-	print("This text is probably not bias")
-if Total = '-7':
-	print("This text is probably not bias")
-if Total = '-6':
-	print("This text is probably not bias")
-if Total = '-5':
-	print("This text is probably not bias")
-if Total = '-4':
-	print("This text is probably not bias")
-if Total = '-3':
-	print("This text is probably not bias")
-if Total = '-2':
-	print("This text is probably not bias")
-if Total = '-1':
-	print("This text is probably not bias")
+print(Total)
+print("If the number is less than -10 the text is probably bias, if the number is more than -10 the text is probably not bias")
