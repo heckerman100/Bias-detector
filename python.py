659ce75e4bc498365723333aaa7bddded4c9794f
Heckerman1000 <huntermcnulty@gmail.com>
2017-11-01 17:23:53 -0400

    Create python.py

---
+++ python.py
@@ -0,0 +1,129 @@
+#This program takes a paragraph of words and adds the numerical value of each word togethor to see if it is bais or not.
+#Not 100% acurate
+
+
+#Non bias words
+they = 0
+male = 0
+female = 0
+buisness = 0
+police = 0
+mother = 0
+father = 0
+brother = 0
+sister = 0
+uncle = 0
+aunt = 0
+grandparents = 0
+grandmother = 0
+grandfather = 0
+we = 0
+server = 0
+elderly = 0
+
+
+#Bias words
+their = 1
+I = 1
+culture = 1
+race = 1
+kingsize = 1
+buisnessman = 1
+career girl = 1
+career woman = 1
+foreman = 1
+mailman = 1
+landlord = 1
+landlady = 1
+newsman = 1
+repairman = 1
+policeman = 1
+salesman = 1
+saleswoman = 1
+saleslady = 1
+cleaning lady = 1
+paper boy = 1
+waitress = 1
+workman = 1
+spokesman = 1
+ladylike = 1
+manly = 1
+maiden = 1
+manhood = 1
+manmade = 1
+motherly = 1
+statesman = 1
+congressman = 1
+congresswoman = 1
+chairmen = 1
+craftsman = 1
+old people = 1
+Eskimo = 1
+fireman = 1
+illegal alien = 1
+Indian = 1
+Jew = 1
+mankind = 1
+underprivileged = 1
+wealthy = 1
+south side = 1
+blue collar = 1
+inner city = 1
+waiter = 1
+
+#Original value
+Original_value = 
+
+#Input text
+Text = input("Paste text or type here: ")
+
+
+
+
+#Applied value
+
+
+
+
+#Total
+Total = Original_value - Applied_value
+
+#Bias
+if Total = '-10':
+	print("This text is probably bias")
+if Total = '-11':
+	Print("This text is probably bias")
+if Total = '-12':
+	Print("This text is probably bias")
+if Total = '-13':
+	Print("This text is probably bias")
+if Total = '-14':
+	Print("This text is probably bias")
+if Total = '-15':
+	Print("This text is probably bias")
+if Total = '-16':
+	Print("This text is probably bias")
+if Total = '-17':
+	Print("This text is probably bias")
+
+
+
+#Non Bias
+if Total = '-9':
+	print("This text is probably not bias")
+if Total = '-8':
+	print("This text is probably not bias")
+if Total = '-7':
+	print("This text is probably not bias")
+if Total = '-6':
+	print("This text is probably not bias")
+if Total = '-5':
+	print("This text is probably not bias")
+if Total = '-4':
+	print("This text is probably not bias")
+if Total = '-3':
+	print("This text is probably not bias")
+if Total = '-2':
+	print("This text is probably not bias")
+if Total = '-1':
+	print("This text is probably not bias")
